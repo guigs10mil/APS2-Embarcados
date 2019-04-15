16cf314613126c31ac81f462dd5424d9375bd0cd
guigs10mil <gui.m.s@hotmail.com>
2019-04-15 15:54:12 -0300

    initial

--- SAME70_FONTS/src/main.c
+++ SAME70_FONTS/src/main.c
@@ -3,8 +3,15 @@
  * APS 2 
  * Rafael . Corsi @ insper . edu . br
  */ 
-#include <stdio.h>
+#include <asf.h>
+#include "tfont.h"
+#include "sourcecodepro_28.h"
+#include "calibri_36.h"
+#include "arial_72.h"
+#include "math.h"
 #include "maquina1.h"
+
+struct ili9488_opt_t g_ili9488_display_opt;
 
 /**
  * Inicializa ordem do menu
@@ -30,7 +37,51 @@ t_ciclo *initMenuOrder(){
   return(&c_diario);
 }
 
+void configure_lcd(void){
+	/* Initialize display parameter */
+	g_ili9488_display_opt.ul_width = ILI9488_LCD_WIDTH;
+	g_ili9488_display_opt.ul_height = ILI9488_LCD_HEIGHT;
+	g_ili9488_display_opt.foreground_color = COLOR_CONVERT(COLOR_WHITE);
+	g_ili9488_display_opt.background_color = COLOR_CONVERT(COLOR_WHITE);
+
+	/* Initialize LCD */
+	ili9488_init(&g_ili9488_display_opt);
+	ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH-1, ILI9488_LCD_HEIGHT-1);
+	
+}
+
+
+void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
+	char *p = text;
+	while(*p != NULL) {
+		char letter = *p;
+		int letter_offset = letter - font->start_char;
+		if(letter <= font->end_char) {
+			tChar *current_char = font->chars + letter_offset;
+			ili9488_draw_pixmap(x, y, current_char->image->width, current_char->image->height, current_char->image->data);
+			x += current_char->image->width + spacing;
+		}
+		p++;
+	}
+}
+
 void main(void){
-  t_ciclo *p_primeiro = initMenuOrder();
-  printf("%s", p_primeiro->next->next->nome);
+	t_ciclo *p_primeiro = initMenuOrder();
+	// printf("%s", p_primeiro->next->next->nome);
+  
+	board_init();
+	sysclk_init();
+  
+	configure_lcd();
+	
+	font_draw_text(&calibri_36, "Velocidade (km/h)", 20, 20, 1);
+	
+	char buffer[32];
+	sprintf(buffer, "%d", 1234567);
+	font_draw_text(&arial_72, buffer, 20, 70, 1);
+	
+	while(1) {
+		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+	}
+
 }
